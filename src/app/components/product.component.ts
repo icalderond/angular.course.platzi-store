e174d496dfd45149fc8700782d8910485df6bcb9
Israel Calderon de la Cruz <icalderond@outlook.com>
2020-04-05 21:58:49 -0500

    Ciclo de vida del componente

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,23 +1,49 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+    Component, 
+    Input, Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChanges, 
+    OnInit, DoCheck, 
+    OnDestroy } from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent{
+export class ProductComponent implements OnInit, DoCheck, OnDestroy{
 
     @Input() product: Product;
     @Output() productAddCart: EventEmitter<any> = new EventEmitter();
-    // product: Product = {
-    //     id: '1',
-    //     image: 'assets/images/camiseta.png',
-    //     title: 'Camiseta',
-    //     price: 80000,
-    //     description: 'Esto es uns descripcion de la camiseta'
-    // };
+
     addCart(){
         console.log('Anadir al carrito');
         this.productAddCart.emit(this.product.id);
     }
+
+    // #region Ciclo de vida de un componente
+    constructor(){
+        console.log('1. Constructor');
+    }
+
+    // Con este se implementa en la clase el OnChanges
+    // ngOnChanges(changes: SimpleChanges){
+    //     console.log('2. ngOnChanges');
+    //     console.log(changes);
+    // }
+
+    ngOnInit(){
+        console.log('3. ngOnInit');
+    }
+
+    // Este metodo no puede trabajar juntp con el metodo ngOnChanges
+    ngDoCheck(){
+        console.log('4. ngDoCheck');
+    }
+
+    ngOnDestroy(){
+        console.log('5. ngOnDestroy');
+    }
+    // #endregion Ciclo de vida de un componente
 }
