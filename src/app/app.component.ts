f0b7bb6363094b34f7f10f691e44b120b61f8116
Israel Calderon de la Cruz <icalderond@outlook.com>
2020-04-05 20:28:36 -0500

    Agregando items a una lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'rodrigo' ];
+
+  addItem(){
+    this.items.push('nuevo item');
+  }
 }
